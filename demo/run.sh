cbcd097c3582155ff8bf83c7a89765f284b5ff2b
William <william@flower.ai>
2025-02-12 16:48:31 -0500

    Update run script

--- demo/run.sh
+++ demo/run.sh
@@ -4,10 +4,10 @@
 RESULTS_DIR="results"
 
 echo "Running Federated Fine-Tuning..."
-# flwr run .
+flwr run .
 
 echo "Running Local Fine-Tuning..."
-python src/fine-tune-local.py
+python src/fine-tune-local.py 
 
 # Find the latest federated fine-tuning run (first folder in sorted list, excluding "_local")
 LATEST_FEDERATED=$(ls -t "$RESULTS_DIR" | grep -v "_local" | head -n 1)
@@ -31,22 +31,38 @@ FEDERATED_PATH="$RESULTS_DIR/$LATEST_FEDERATED/peft_100/"
 LOCAL_PATH="$RESULTS_DIR/$LATEST_LOCAL/checkpoint-100/"
 
 echo "Evaluating Federated Fine-Tuning..."
-#python ./src/benchmarks/general-nlp/eval.py \
-#  --base-model-name-path=Qwen/Qwen2-0.5B-Instruct \
-#  --peft-path="$FEDERATED_PATH" \
-#  --run-name=qwen-fed \
-#  --batch-size=16 \
-#  --quantization=4 \
-#  --category=stem,social_sciences
+python ./src/benchmarks/general-nlp/eval.py \
+  --base-model-name-path=Qwen/Qwen2-0.5B-Instruct \
+  --peft-path="$FEDERATED_PATH" \
+  --run-name=qwen-fed \
+  --batch-size=16 \
+  --quantization=4 \
+  --category=stem,social_sciences
+
+python ./src/benchmarks/general-nlp/eval.py \
+  --base-model-name-path=Qwen/Qwen2-0.5B-Instruct \
+  --peft-path="$FEDERATED_PATH" \
+  --run-name=qwen-fed \
+  --batch-size=16 \
+  --quantization=4 \
+  --category=stem,social_sciences
+
+python ./src/benchmarks/finance/eval.py \
+  --base-model-name-path=Qwen/Qwen2-0.5B-Instruct \
+  --peft-path="$FEDERATED_PATH" \
+  --run-name=qwen-fed \
+  --batch-size=16 \
+  --quantization=4 \
+  --datasets=fpb,fiqa,tfns
 
 echo "Evaluating Local Fine-Tuning..."
-python ./src/benchmarks/general-nlp/eval.py \
+python ./src/benchmarks/finance/eval.py \
   --base-model-name-path=Qwen/Qwen2-0.5B-Instruct \
   --peft-path="$LOCAL_PATH" \
   --run-name=qwen-local \
   --batch-size=16 \
   --quantization=4 \
-  --category=stem,social_sciences
+  --datasets=fpb,fiqa,tfns
 
 echo "Generating Visualizations..."
 python ./src/plot_results.py  # Adjust if necessary
